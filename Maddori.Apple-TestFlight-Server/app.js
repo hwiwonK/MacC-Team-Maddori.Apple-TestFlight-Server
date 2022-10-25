c206474e23c8226c921432aa6fcaa01d2ba42004
hwiwonK <gnldnjs2469@gmail.com>
2022-10-25 13:01:23 +0900

    [SETTING] 데이터베이스 세팅

--- Maddori.Apple-TestFlight-Server/app.js
+++ Maddori.Apple-TestFlight-Server/app.js
@@ -1,8 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const { sequelize } = require('./models');
+
+sequelize.sync({ force: false })
+.then(() => {
+    console.log('데이터베이스 연결 성공');
+})
+.catch((err) => {
+    console.log('데이터베이스 연결 실패');
+    console.error(err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+});
   
-  module.exports = app
+module.exports = app;
