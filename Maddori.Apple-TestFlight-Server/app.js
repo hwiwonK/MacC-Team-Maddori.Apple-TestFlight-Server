8e8360d61c9ab72022f942fc0661b2ffe21481a0
hwiwonK <gnldnjs2469@gmail.com>
2022-10-25 17:48:07 +0900

    [Chore] console.log 및 불필요한 코드 제거

--- Maddori.Apple-TestFlight-Server/app.js
+++ Maddori.Apple-TestFlight-Server/app.js
@@ -5,17 +5,10 @@ const models = require("./models/index.js");
 
 app.use(express.json());
 
-sequelize.sync({ force: false })
-.then(() => {
-    console.log('데이터베이스 연결 성공');
-})
-.catch((err) => {
-    console.log('데이터베이스 연결 실패');
-    console.error(err);
-});
+sequelize.sync({ force: false });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('This is maddori-apple testflight server');
 });
 
 app.use('/css', require('./routes/css'));
--- Maddori.Apple-TestFlight-Server/routes/css.js
+++ Maddori.Apple-TestFlight-Server/routes/css.js
@@ -5,7 +5,6 @@ const {css} = require('../models');
 
 //키워드 정보 가져오기
 router.get('/keywords', async function(req, res, next) {
-    console.log("키워드 가져오기");
     try {
         const keywordList = await css.findAll({
             attributes: ['keyword'],
@@ -16,33 +15,22 @@ router.get('/keywords', async function(req, res, next) {
             keywords : keywordList.map((data) => data.keyword)
         }
         res.status(200).send(result);
-        console.log("키워드 가져오기 성공");
-        console.log(result);
     } catch (e) {
         res.status(500).send(e);
-        console.log("키워드 가져오기 실패");
-        console.log(e);
     }
 });
 
 //css 등록
 router.post('/', async function(req, res, next) {
-    console.log("css 등록");
     const cssContent = req.body;
-    console.log(cssContent);
     
     try {
         const createdCss = await css.create(cssContent);
         res.status(201).send(createdCss);
-        console.log("css 등록 성공");
-        console.log(createdCss);
     }
     catch (e) {
         res.status(500).send(e);
-        console.log("css 등록 실패");
-        console.log(e);
     }
 });
-
 
 module.exports = router;
